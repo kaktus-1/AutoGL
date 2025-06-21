14927db6e68b6a6a5e40f9ca3728fc6d65637a12
kaktus-1 <56768074+kaktus-1@users.noreply.github.com>
2025-06-21 16:29:14 +0200

    Removed the usage of #stream() to match the labymod guidelines

--- core/src/main/java/de/kaktus4/autogl/core/server/ServerConfigurationImpl.java
+++ core/src/main/java/de/kaktus4/autogl/core/server/ServerConfigurationImpl.java
@@ -1,36 +1,33 @@
 package de.kaktus4.autogl.core.server;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class ServerConfigurationImpl implements ServerConfiguration {
 
-    private final List<String> serverAddresses;
-    private final List<String> triggerMessages;
-    private final List<String> exclusionFilters;
+  private final List<String> serverAddresses;
+  private final List<String> triggerMessages;
+  private final List<String> exclusionFilters;
 
-    public ServerConfigurationImpl(List<String> addresses, List<String> formats, List<String> filters) {
-        this.serverAddresses = List.copyOf(
-            addresses.stream()
-                     .map(String::toLowerCase)
-                     .collect(Collectors.toList())
-        );
-        this.triggerMessages = List.copyOf(formats);
-        this.exclusionFilters = List.copyOf(filters);
-    }
+  public ServerConfigurationImpl(List<String> addresses, List<String> formats,
+      List<String> filters) {
+    this.serverAddresses = List.copyOf(
+        addresses); // Should be normally lower-case because of hardcoded implementation
+    this.triggerMessages = List.copyOf(formats);
+    this.exclusionFilters = List.copyOf(filters);
+  }
 
-    @Override
-    public List<String> getServerAddresses() {
-        return serverAddresses;
-    }
+  @Override
+  public List<String> getServerAddresses() {
+    return serverAddresses;
+  }
 
-    @Override
-    public List<String> getTriggerMessages() {
-        return triggerMessages;
-    }
+  @Override
+  public List<String> getTriggerMessages() {
+    return triggerMessages;
+  }
 
-    @Override
-    public List<String> getExclusionFilters() {
-        return exclusionFilters;
-    }
+  @Override
+  public List<String> getExclusionFilters() {
+    return exclusionFilters;
+  }
 }
